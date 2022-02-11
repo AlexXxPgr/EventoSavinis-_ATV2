16711a92f66fd8686c2bef105ba483c321a284e3
G. Alex <97766420+AlexXxPgr@users.noreply.github.com>
2022-02-10 23:11:18 -0300

    Add files via upload

---
+++ EventoSavinis.js
@@ -0,0 +1,28 @@
+console.log("HELLO WORLD !")
+let DataEvento = new Date(02/01/2023) 
+let DataAtual = new Date()
+
+if(DataAtual > DataEvento) {
+    console.log ("Data selecionada")
+
+    let idade = 19
+
+    if(idade >= 18){
+    console.log("Idade registrada com sucesso !")
+    } 
+    else{
+        console.log("Idade inválida")
+    }
+    let Participantes = ['alex', 'samara', 'caio', 'chico', 'cavalcanti']
+    let Palestrantes = ['Ana', 'Ygor', 'Lucas', 'Augusto']
+if(Participantes.length, Palestrantes.length <= 100){
+    console.log(`${Participantes} ${Palestrantes} Cadastro realizado !`)
+}
+else{
+    console.log("Capacidade máxima atingida")
+}
+} 
+else{
+    console.log("Data inválida")
+}
+
